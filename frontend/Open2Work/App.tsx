a816b6f50cc8ffd1fbbd048fbb6fc8fd3a88f776
NaFaPaz <nfanego@gmail.com>
2022-09-28 00:30:10 -0300

    [Add] New Inputs to Register

--- frontend/Open2Work/App.tsx
+++ frontend/Open2Work/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
-import { Provider as PaperProvider, DefaultTheme } from 'react-native-paper';
-import { LoginScreen } from './src/screens/LoginScreen'
+import React from 'react';
+import {Provider as PaperProvider, DefaultTheme} from 'react-native-paper';
+import {LoginScreen} from './src/screens/LoginScreen';
+import Register from './src/screens/Register';
+import {SafeAreaView} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const theme = {
   ...DefaultTheme,
@@ -12,16 +15,23 @@ const theme = {
     background: '#39304d',
     surface: '#1f1a30',
     text: '#fff',
-    placeholder: '#fff'
+    placeholder: '#fff',
   },
 };
 
 const App = () => {
   return (
-    <PaperProvider theme={ theme }>
-      <LoginScreen />
+    <PaperProvider
+      theme={theme}
+      settings={{
+        icon: props => <Icon {...props} />,
+      }}>
+      <SafeAreaView style={{height: '100%'}}>
+        <LoginScreen />
+        <Register />
+      </SafeAreaView>
     </PaperProvider>
-  )
-}
+  );
+};
 
 export default App;
--- frontend/Open2Work/src/screens/Register.tsx
+++ frontend/Open2Work/src/screens/Register.tsx
@@ -1,52 +1,201 @@
-import * as React from 'react';
-import {Text, View, Button, ScrollView} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, ScrollView} from 'react-native';
 import {Formik} from 'formik';
-import {TextInput} from 'react-native-paper';
-
-const MyInput = ({...props}) => {
-  return (
-    <TextInput
-      style={{
-        backgroundColor: 'rgb(57, 48, 77)',
-        marginVertical: 5,
-        marginHorizontal: 10,
-        borderBottomColor: 'rgb(24, 241, 222)',
-      }}
-      theme={{
-        colors: {
-          text: 'white',
-          placeholder: 'rgb(205,205,205)',
-          primary: 'rgb(24, 241, 222)',
-        },
-      }}
-      {...props}
-    />
-  );
-};
+import {MyInput} from '../components/MyInput';
+import {Avatar, Headline, Button} from 'react-native-paper';
+import {List} from 'react-native-paper';
 
 const Register = () => {
+  const [expanded, setExpanded] = useState(false);
+  const [selected, setSelected] = useState('');
+  const handlePress = () => {
+    setExpanded(!expanded);
+  };
   return (
     <ScrollView
       contentContainerStyle={{
-        backgroundColor: 'rgb(31, 26, 48)',
-        height: '100%',
+        backgroundColor: 'rgb(31,26,48)',
+        alignItems: 'center',
       }}>
-      <Text style={{color: 'white'}}>Create Account</Text>
+      <Headline
+        style={{
+          color: 'white',
+          marginTop: 20,
+          fontWeight: '700',
+          fontSize: 30,
+        }}>
+        Create Account
+      </Headline>
+      <View style={{marginTop: 25}}>
+        <Avatar.Icon size={95} icon="person-outline" />
+        <Text
+          style={{
+            left: 70,
+            bottom: 30,
+            color: 'white',
+            borderRadius: 50,
+            width: 34,
+            fontSize: 25,
+            textAlign: 'center',
+            backgroundColor: 'darkgrey',
+          }}>
+          +
+        </Text>
+      </View>
       <Formik
-        initialValues={{email: '', pass: '', name: ''}}
+        initialValues={{
+          role: '',
+          last: '',
+          pass: '',
+          name: '',
+          stack: '',
+          language: '',
+          location: '',
+        }}
         onSubmit={values => console.log(values)}>
-        {({handleChange, handleBlur, handleSubmit, values}) => (
-          <View style={{minHeight: '100%'}}>
+        {({handleChange, handleSubmit, values}) => (
+          <View style={{minHeight: '100%', width: '80%', marginBottom: 25}}>
             <MyInput
-              label={'E-mail'}
+              iconName="person-outline"
+              label={'First Name'}
+              value={values.name}
+              onChangeText={handleChange('name')}
+            />
+            <MyInput
+              iconName="person-add-outline"
+              label={'Last Name'}
+              value={values.last}
+              onChangeText={handleChange('last')}
+            />
+            <List.Section style={{borderRadius: 50}}>
+              <List.Accordion
+                title="Role"
+                left={props => (
+                  <List.Icon {...props} color="white" icon="build-outline" />
+                )}
+                right={props => (
+                  <List.Icon
+                    {...props}
+                    icon={
+                      expanded ? 'caret-down-outline' : 'caret-forward-outline'
+                    }
+                  />
+                )}
+                expanded={expanded}
+                onPress={handlePress}
+                style={{borderRadius: 10}}>
+                <View
+                  style={{
+                    borderRadius: 5,
+                    borderWidth: 1,
+                    borderColor: 'grey',
+                  }}>
+                  <List.Item
+                    title="Front End"
+                    left={props => (
+                      <List.Icon
+                        {...props}
+                        icon={
+                          selected === 'front'
+                            ? 'radio-button-on-outline'
+                            : 'radio-button-off-outline'
+                        }
+                      />
+                    )}
+                    onPress={() => setSelected('front')}
+                  />
+                  <List.Item
+                    title="Back End"
+                    left={props => (
+                      <List.Icon
+                        {...props}
+                        icon={
+                          selected === 'back'
+                            ? 'radio-button-on-outline'
+                            : 'radio-button-off-outline'
+                        }
+                      />
+                    )}
+                    onPress={() => setSelected('back')}
+                  />
+                  <List.Item
+                    title="UIX"
+                    left={props => (
+                      <List.Icon
+                        {...props}
+                        icon={
+                          selected === 'uix'
+                            ? 'radio-button-on-outline'
+                            : 'radio-button-off-outline'
+                        }
+                      />
+                    )}
+                    onPress={() => setSelected('uix')}
+                  />
+                  <List.Item
+                    title="QA"
+                    left={props => (
+                      <List.Icon
+                        {...props}
+                        icon={
+                          selected === 'qa'
+                            ? 'radio-button-on-outline'
+                            : 'radio-button-off-outline'
+                        }
+                      />
+                    )}
+                    onPress={() => setSelected('qa')}
+                  />
+                </View>
+              </List.Accordion>
+            </List.Section>
+
+            <MyInput
+              label={'Tech Stack'}
+              value={values.stack}
+              onChangeText={handleChange('stack')}
+            />
+            <MyInput
+              label={'Location'}
+              value={values.location}
+              onChangeText={handleChange('location')}
+            />
+            <MyInput
+              label={'Language'}
+              value={values.language}
+              onChangeText={handleChange('language')}
+            />
+            <MyInput
+              label={'Language'}
               value={values.email}
-              onChangeText={handleChange('email')}
+              onChangeText={handleChange('language')}
+            />
+            <MyInput
+              label={'Language'}
+              value={values.email}
+              onChangeText={handleChange('language')}
+            />
+            <MyInput
+              label={'Language'}
+              value={values.email}
+              onChangeText={handleChange('language')}
             />
             <Button
-              color={'rgb(24, 241, 222)'}
               onPress={handleSubmit}
-              title="Submit"
-            />
+              mode="contained"
+              style={{
+                width: '60%',
+                alignSelf: 'center',
+                marginTop: 20,
+                borderRadius: 40,
+              }}>
+              <Text
+                style={{
+                  fontSize: 25,
+                }}>
+                SUBMIT
+              </Text>
+            </Button>
           </View>
         )}
       </Formik>
