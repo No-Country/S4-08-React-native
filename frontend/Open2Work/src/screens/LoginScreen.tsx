cea541ad5dee9cf845f92b390c6ae9f65fb75ea5
Abel Aron Amieva <abelamieva@gmail.com>
2022-09-30 17:50:35 -0300

    tipado al Screen

--- frontend/Open2Work/src/screens/LoginScreen.tsx
+++ frontend/Open2Work/src/screens/LoginScreen.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { ScrollView, View, TouchableWithoutFeedback, Keyboard, StatusBar } from 'react-native';
+import { StackScreenProps } from '@react-navigation/stack';
 import { ImageLogin } from '../components/ImageLogin';
 import { FormLogin } from '../components/FormLogin';
 import { FormRegister } from '../components/FormRegister';
+import { RootStackParamList } from '../navigation/Navigation';
 
+type Props = StackScreenProps<RootStackParamList,'LoginScreen'>
 
-
-export const LoginScreen = () => {
+export const LoginScreen = ({ navigation }: Props) => {
 
     const [isRegister, setIsRegister] = useState(true);
 
@@ -31,7 +33,6 @@ export const LoginScreen = () => {
                             paddingHorizontal: 20,
                             top: -30,
                             justifyContent: 'space-between',
-                            // height: 600
                         }}
                     >
                         {
@@ -40,6 +41,7 @@ export const LoginScreen = () => {
                         
                     </View>
                 </>
+                
             </TouchableWithoutFeedback>
         </ScrollView>
     )
