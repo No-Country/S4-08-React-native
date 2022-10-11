6ca603866e9089fabf6970a635ee86375aa7840b
Abel Aron Amieva <abelamieva@gmail.com>
2022-10-11 15:12:50 -0300

    se agrega la validacion de token para el redireccionamiento

--- frontend/Open2Work/src/navigation/Navigation.tsx
+++ frontend/Open2Work/src/navigation/Navigation.tsx
@@ -1,33 +1,50 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-import {createStackNavigator} from '@react-navigation/stack';
-import {LoginScreen} from '../screens/LoginScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import { LoginScreen } from '../screens/LoginScreen';
 import Register from '../screens/Register';
 import IsDev from '../screens/IsDev';
 import Profile from '../screens/Profile';
-import {TabNav} from './TabNav';
+import { useAppSelector } from '../redux/hook';
+import { TabNavDev } from './TabNavDev';
+import { TabNavClient } from './TabNavClient';
 
 export type RootStackParamList = {
   LoginScreen: undefined;
   IsDev: undefined;
-  Register: {isDev: boolean};
-  Profile: undefined;
+  Register: { isDev: boolean };
+  Profile: { render: string };
   TabNav: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 export const StackNavigation = () => {
+
+  const { auth, user } = useAppSelector(state => state);
+
+  const { isLoading, token } = auth;
+  const { isDev } = user;
+
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
       }}>
-      <Stack.Screen name="TabNav" component={TabNav} />
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="LoginScreen" component={LoginScreen} />
-      <Stack.Screen name="IsDev" component={IsDev} />
-      <Stack.Screen name="Register" component={Register} />
+      {
+        token === null ? (
+          <>
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen name="IsDev" component={IsDev} />
+            <Stack.Screen name="Register" component={Register} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen name="TabNav" component={ isDev ? TabNavDev : TabNavClient} />
+            {/* <Stack.Screen name="Profile" component={Profile} /> */}
+          </>
+        )
+      }
     </Stack.Navigator>
   );
 };
