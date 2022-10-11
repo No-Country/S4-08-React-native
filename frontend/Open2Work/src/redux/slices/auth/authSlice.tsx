52691755ff4f42b0562216d0ae0797b6163d0bf8
Abel Aron Amieva <abelamieva@gmail.com>
2022-10-11 17:04:56 -0300

    se establecio la accion de logout en el reducer

--- frontend/Open2Work/src/redux/slices/auth/authSlice.tsx
+++ frontend/Open2Work/src/redux/slices/auth/authSlice.tsx
@@ -13,7 +13,6 @@ const initialState: AuthState ={
 
 export const authSlice = createSlice({
     name: 'auth',
-    // `createSlice` will infer the state type from the `initialState` argument
     initialState,
     reducers: {
         setToken: (state: AuthState, action): AuthState => {
@@ -23,22 +22,12 @@ export const authSlice = createSlice({
                 token: action.payload
             }
         },
-        logout:(state: AuthState, action): AuthState => {
+        logout:(state: AuthState ): AuthState => {
             return {
                 ...state,
                 token: null
             }
         }
-        // increment: state => {
-        //   state.value += 1
-        // },
-        // decrement: state => {
-        //   state.value -= 1
-        // },
-        // // Use the PayloadAction type to declare the contents of `action.payload`
-        // incrementByAmount: (state, action: PayloadAction<number>) => {
-        //   state.value += action.payload
-        // }
     }
 })
 
