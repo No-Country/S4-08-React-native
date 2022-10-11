0b8579ccc02a2ddfc7e702b19fc17e91f002a32c
Abel Aron Amieva <abelamieva@gmail.com>
2022-10-11 17:05:07 -0300

    se creo el slice para los errores

---
+++ frontend/Open2Work/src/redux/slices/error/errorSlice.tsx
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { RootState } from '../../store';
+
+interface ErrorState {
+    error: string | null,
+    showModal: boolean
+}
+
+const initialState: ErrorState ={
+    error: null,
+    showModal: false
+}
+
+export const errorSlice = createSlice({
+    name: 'error',
+    // `createSlice` will infer the state type from the `initialState` argument
+    initialState,
+    reducers: {
+        setError: (state: ErrorState, action): ErrorState => {
+            return {
+                ...state,
+                error: action.payload,
+                showModal: true
+            }
+        },
+        removeError: (state: ErrorState ): ErrorState => {
+            return {
+                ...state,
+                showModal: false,
+                error: null
+            }
+        }
+    }
+})
+
+export const { setError, removeError } = errorSlice.actions
+
+// Other code such as selectors can use the imported `RootState` type
+export const selectError = (state: RootState) => state.register;
+
+export default errorSlice.reducer;
