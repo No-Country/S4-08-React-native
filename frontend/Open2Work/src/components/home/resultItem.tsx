ad5677e2e8ce15e893cc01c3577785384a097c97
Abel Aron Amieva <abelamieva@gmail.com>
2022-10-17 18:40:11 -0300

    Implementando la navegacion al GroupDetails

--- frontend/Open2Work/src/components/home/resultItem.tsx
+++ frontend/Open2Work/src/components/home/resultItem.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
 
-const ResultItem = ({data}) => {
+const ResultItem = ({ data }) => {
+
+  const navigation = useNavigation();
+  
   return (
     <View
       style={{
@@ -14,28 +20,35 @@ const ResultItem = ({data}) => {
         justifyContent: 'space-between',
         flexWrap: 'wrap',
       }}>
-      <Text
-        style={{
-          flex: 1,
-          flexBasis: '100%',
-          flexGrow: 1,
-          color: '#17f1de',
-          paddingBottom: 5,
-        }}>
-        GROUP #{data._id.slice(-4)}
-      </Text>
-      <Text style={{color: 'white', marginBottom: 5, marginRight: 15}}>
+      <TouchableOpacity
+        onPress={()=> navigation.navigate('Group', {
+          id: data._id
+        })}
+      >
+        <Text
+          style={{
+            flex: 1,
+            flexBasis: '100%',
+            flexGrow: 1,
+            color: '#17f1de',
+            paddingBottom: 5,
+          }}>
+          GROUP #{data._id.slice(-4)}
+        </Text>
+
+      </TouchableOpacity>
+      <Text style={{ color: 'white', marginBottom: 5, marginRight: 15 }}>
         <Icon name="build-outline" size={16} /> {data.stack}
       </Text>
-      <Text style={{color: 'lightgrey'}}>
+      <Text style={{ color: 'lightgrey' }}>
         <Icon name="language-outline" size={16} /> [
-        {data.language.reduce((lang0, lang1) => `${lang0} - ${lang1}`,'')}]
+        {data.language.reduce((lang0, lang1) => `${lang0} - ${lang1}`, '')}]
       </Text>
-      <Text style={{color: 'lightgrey'}}>
+      <Text style={{ color: 'lightgrey' }}>
         <Icon name="time-outline" size={16} /> [
-        {data.time_zone.reduce((tz0, tz1) => `${tz0} / ${tz1}`, )}]
+        {data.time_zone.reduce((tz0, tz1) => `${tz0} / ${tz1}`,)}]
       </Text>
-      <Text style={{color: 'lightgrey'}}>
+      <Text style={{ color: 'lightgrey' }}>
         <Icon name="briefcase-outline" size={16} /> {data.availability || ''}
       </Text>
     </View>
